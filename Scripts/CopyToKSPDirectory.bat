325e0d70badd387b777a501fde30bcbb6946d75d
David Federspiel <dfederspiel@apterainc.com>
2019-07-30 23:27:58 -0400

    updated batch file to copy assets

--- Scripts/CopyToKSPDirectory.bat
+++ Scripts/CopyToKSPDirectory.bat
@@ -5,20 +5,19 @@
 ::EXAMPLE:
 :: SET KSPPATH=C:\Program Files (x86)\Steam\steamapps\common\Kerbal Space Program
 :: SET KSPPATH2=C:\Users\Malte\Desktop\Kerbal Space Program
-call ".\SetDirectories.bat"
-set PLUGIN_NAME="Haystack"
+set KSPPATH=C:\Kerbal Space Program\KSP 1.7.3 Developer
+set PLUGIN_NAME=HaystackContinued
 
-IF DEFINED KSPPATH (ECHO KSPPATH is defined) ELSE (SET KSPPATH=C:\Kerbal Space Program)
-::%1
-SET SOLUTIONCONFIGURATION=Debug
+set SOLUTIONCONFIGURATION=Debug
 
 mkdir "%KSPPATH%\GameData\%PLUGIN_NAME%\"
 
 mkdir "%KSPPATH%\GameData\%PLUGIN_NAME%\Plugins"
-IF DEFINED KSPPATH2 (mkdir "%KSPPATH2%\GameData\%PLUGIN_NAME%\Plugins")
 
 del "%KSPPATH%\GameData\%PLUGIN_NAME%\Plugins\*.*" /Q /F
 
 "%~dp0..\External\pdb2mdb\pdb2mdb.exe" "%~dp0..\HaystackContinued\bin\%SOLUTIONCONFIGURATION%\HaystackContinued.dll"
 
-xcopy /Y "%~dp0..\HaystackContinued\bin\%SOLUTIONCONFIGURATION%\*.*" "%KSPPATH%\GameData\%PLUGIN_NAME%\Plugins"
+xcopy /Y /O /X /E /H /K "%~dp0..\HaystackContinued\bin\%SOLUTIONCONFIGURATION%\*.*" "%KSPPATH%\GameData\%PLUGIN_NAME%\Plugins"
+
+xcopy /Y /O /X /E /H /K "%~dp0..\GameData\HaystackContinued\*.*" "%KSPPATH%\GameData\%PLUGIN_NAME%"
